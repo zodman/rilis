6406e5f4fd20cb86b0528f3f20ba79758f8eb206
zodman <zodman@gmail.com>
2018-05-12 23:23:56 -0500

    enhanced yaml conf location

--- rilis/deploy.bat
+++ rilis/deploy.bat
@@ -1,5 +1,6 @@
 pyinstaller -F rilis.py
 copy conf.yaml.ini dist\conf.yaml
 copy README.md dist\
-7z a rilis.zip dist\
-curl -T rilis.zip transfer.sh -k -L
+
+#7z a rilis.zip dist\
+#curl -T rilis.zip transfer.sh -k -L
--- rilis/utils.py
+++ rilis/utils.py
@@ -2,13 +2,19 @@ import os
 import yaml
 import click
 import sys
-ROOT_DIR = BASE_DIR = os.path.dirname(sys.argv[0])
+
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
+
+ROOT_DIR = BASE_DIR = application_path
 
 def get_conf(section=None):
     path_file = os.path.join(ROOT_DIR, "conf.yaml")
     click.echo("try to open {}".format(path_file))
     if not os.path.exists(path_file):
-        raise Exception("File  yaml not found")
+        raise Exception("File  {} not found".format(path_file))
     content = yaml.load(open(path_file).read())
     if not section:
         return content
