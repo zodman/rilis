5d1620d66a781f0f4e3afc1cfe444cbb3c1e8324
Andres Vargas <zodman@gmail.com>
2018-05-14 18:43:40 -0500

    colorsito

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ ROOT_DIR = BASE_DIR = application_path
 
 def get_conf(section=None):
     path_file = os.path.join(ROOT_DIR, "conf.yaml")
-    click.echo("try to open {}".format(path_file))
+    #click.echo("try to open {}".format(path_file))
     if not os.path.exists(path_file):
         raise Exception("File  {} not found".format(path_file))
     content = yaml.load(open(path_file).read())
