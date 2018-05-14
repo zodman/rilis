d06248d44d45341b962b1c496ac952dc87f49182
Andres Vargas <zodman@gmail.com>
2018-05-14 17:56:02 -0500

    reindent task

--- tasks.py
+++ tasks.py
@@ -2,12 +2,12 @@ from invoke import task
 
 @task
 def deploy(ctx, up=False):
-	"""
-		script para generar el exe y subir a transfer para compartir
-	"""
-	ctx.run("pyinstaller -F rilis.py")
-	ctx.run("copy conf.yaml.ini dist\conf.yaml")
-	ctx.run("copy README.md dist\\ ")
-	if up:
-		ctx.run("7z a rilis.zip dist\\")
-		ctx.run("curl --progress-bar -T rilis.zip transfer.sh -k -L")
+    """
+        script para generar el exe y subir a transfer para compartir
+    """
+    ctx.run("pyinstaller -F rilis.py")
+    ctx.run("copy conf.yaml.ini dist\conf.yaml")
+    ctx.run("copy README.md dist\\ ")
+    if up:
+        ctx.run("7z a rilis.zip dist\\")
+        ctx.run("curl --progress-bar -T rilis.zip transfer.sh -k -L")
