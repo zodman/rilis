b76910bfb5babe29a9f1b78a797de9bbc79ba27f
zodman <zodman@gmail.com>
2018-05-14 20:52:21 -0500

    update changes

--- tasks.py
+++ tasks.py
@@ -1,13 +1,27 @@
 from invoke import task
 
 @task
+def compile_mktorrent(ctx):
+	with ctx.cd("mktorrent"):
+		ctx.run("pyinstaller -F createtorrent.py")
+	ctx.run("copy mktorrent\\dist\\createtorrent.exe dist\\ ")
+
+
+@task
 def deploy(ctx, up=False):
 	"""
 		script para generar el exe y subir a transfer para compartir
 	"""
-	ctx.run("pyinstaller -F rilis.py")
+	cmd =[
+	"pyinstaller",
+	'--hidden-import','grab.transport',
+	'-F'
+	'rilis.py'
+	]
+	ctx.run(" ".join(cmd))
 	ctx.run("copy conf.yaml.ini dist\conf.yaml")
 	ctx.run("copy README.md dist\\ ")
+
 	if up:
 		ctx.run("7z a rilis.zip dist\\")
 		ctx.run("curl --progress-bar -T rilis.zip transfer.sh -k -L")
